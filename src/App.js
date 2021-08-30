9904bbb0a3d5faa4d4488801ff273d5c3d648507
TahhRahh <1234tararyan@gmail.com>
2021-08-30 14:44:41 +0100

    built form & search box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        
-
 <h1>Dictionary App</h1>
-
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        This page was coded by Tara Ryan and is open sourced.
+      </footer>
+    </div>
     </div>
   );
 }
